b72926da717459d48c7d09cef0fa808cada24eda
micha <michal.skrobucha@gmail.com>
2025-04-04 21:05:31 +0200

    Algorytmy zachłanny i przeglądu wszystkich możliwych kombinacji

--- solver/solverLogic.py
+++ solver/solverLogic.py
@@ -1,3 +1,4 @@
+from itertools import chain, combinations
 
 
 class solverLogic:
@@ -5,14 +6,41 @@ class solverLogic:
         self.capacity: float = 0.0
         self.items: list[list[float]] = []  # [value, weight]
 
-    def loadData(self, capacity : float, items : list[list[float]]) -> None:
+    def loadData(self, capacity: float, items: list[list[float]]) -> None:
         self.capacity = capacity
         self.items = items
 
     def precise(self) -> (float, list[float]):
-        print('solving with precise algorithm')
-        return self.capacity, self.items
+        subsets: list[list[list[float]]] = list(
+            chain.from_iterable(combinations(self.items, r) for r in range(len(self.items) + 1)))
+
+        maxValue: float = 0.0
+        chosen: list[list[float]] = []
+
+        for s in subsets:
+            weight: float = 0.0
+            value: float = 0.0
+
+            for v, w in s:
+                weight += w
+                value += v
+
+            if weight <= self.capacity and value > maxValue:
+                chosen = s
+                maxValue = value
+
+        return maxValue, chosen
 
     def greedy(self) -> (float, list[float]):
-        print('solving with greedy algorithm')
-        return 0.0, []
+        value: float = 0.0
+        weight: float = 0.0
+        densities: list[list[float]] = sorted([[v, w, v / w] for v, w in self.items], key=lambda x: x[2], reverse=True)
+        chosen: list[list[float]] = []
+
+        for v, w, d in densities:
+            if weight + w <= self.capacity:
+                chosen.append([v, w])
+                value += v
+                weight += w
+
+        return value, chosen
